5e5de8abf84523b3e659369770236e96665ec25d
Kwon <ggiicchh48@gmail.com>
2023-11-22 04:37:29 +0900

    bug fix

--- src/components/MyStocks.jsx
+++ src/components/MyStocks.jsx
@@ -11,15 +11,21 @@ export default function GetMyStocks({ address, upDateBalance }) {
 
   const getStocks = async () => {
     const stocks = await getStocksByAddress(address);
+    let stockOutbalan;
     const balanStock = stocks.filter(
       (el) =>
         el.contractAddress === "0xd8cb493ab47d173cb7ecf9952f39fa78055b775d"
     );
-    const stockOutbalan = stocks.filter(
-      (el) =>
-        el.contractAddress !== "0xd8cb493ab47d173cb7ecf9952f39fa78055b775d"
-    );
-    if (balanStock) upDateBalance(parseInt(balanStock[0].balance));
+    if (balanStock.length !== 0) {
+      stockOutbalan = stocks.filter(
+        (el) =>
+          el.contractAddress !== "0xd8cb493ab47d173cb7ecf9952f39fa78055b775d"
+      );
+      upDateBalance(parseInt(balanStock[0].balance));
+    } else {
+      stockOutbalan = stocks;
+    }
+    console.log(balanStock);
     setTokens(stockOutbalan.slice(0, 5));
   };
 
