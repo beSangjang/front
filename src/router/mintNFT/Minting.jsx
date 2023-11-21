018e3df844873535daa6d2c6c8258076abb151cd
redfl0wer <lion3032@naver.com>
2023-11-21 20:33:04 +0900

    add google survey

--- src/router/mintNFT/Minting.jsx
+++ src/router/mintNFT/Minting.jsx
@@ -54,7 +54,7 @@ export default function MintingPage() {
 
   return (
     <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSdnNK-18zAdW1NQXKqA2fzezO4pc6gL0WlX1ElEhVvMHnyUAg/viewform?embedded=true"
+      src="https://docs.google.zezcom/forms/d/e/1FAIpQLSdnNK-18zAdW1NQXKqA2fO4pc6gL0WlX1ElEhVvMHnyUAg/viewform?embedded=true"
       width="640"
       height="700"
       frameborder="0"
