803098764476a61959371c4853347775c51678cc
redfl0wer <lion3032@naver.com>
2023-11-23 19:00:45 +0900

    accordion

--- src/router/mintNFT/Minting.jsx
+++ src/router/mintNFT/Minting.jsx
@@ -1,67 +1,69 @@
 import React, { useState } from "react";
-import { jsonToPinata } from "../../api/pinataCall.js";
-import { uploadImgToPinata } from "../../api/pinataCall.js";
-import { minting, sendSignTx } from "../../contract/minting.js";
 
-const FormData = require("form-data");
+const Accordion = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default function MintingPage() {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [prevImage, setPrevImage] = useState(null);
-  const [nftName, setNftName] = useState("");
-  const [nftDescription, setNftDescription] = useState("");
-  const [mintingStatus, setMintingStatus] = useState("");
-
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-
-    const reader = new FileReader();
-    setSelectedImage(file);
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setPrevImage(reader.result);
-    };
-  };
-
-  const mintNFT = async () => {
-    // 1. 이미지를 Pinata에 업로드합니다.
-    const formData = new FormData();
-    formData.append("file", selectedImage);
-    const ipfsHash = await uploadImgToPinata(formData);
-    console.log(ipfsHash);
-    // 2. NFT 메타데이터를 작성합니다.
-    const metadata = {
-      name: nftName,
-      description: nftDescription,
-      image: ipfsHash,
-    };
-    jsonToPinata(metadata);
-
-    // 3. NFT를 생성합니다.
-    const tokenURI = await jsonToPinata(metadata);
-    const address = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    console.log(address[0]);
-    if (tokenURI) {
-      const result = await minting(tokenURI, address[0]);
-      if (result.hash) {
-        await result.wait();
-        sendSignTx(result.hash);
-      }
-    }
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSdnNK-18zAdW1NQXKqA2fzezO4pc6gL0WlX1ElEhVvMHnyUAg/viewform?embedded=true"
-      width="640"
-      height="700"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
-    >
-      로드 중…
-    </iframe>
+    <div className={`max-w-md mx-auto mt-8 ${isOpen ? "expanded-bg" : ""}`}>
+      <div
+        className={`bg-white shadow-md rounded-md ${isOpen ? "expanded" : ""}`}
+      >
+        <div
+          className="flex items-center justify-between p-4 cursor-pointer"
+          onClick={toggleAccordion}
+        >
+          <h2 className="text-lg font-semibold">wesfrtafsefeswd</h2>
+          <svg
+            className={`w-6 h-6 ${isOpen ? "transform rotate-180" : ""}`}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </div>
+        {isOpen && (
+          <div className="p-4">
+            <p>jwoirghykijrgokjkiol</p>
+          </div>
+        )}
+      </div>
+    </div>
   );
-}
+};
+
+const GoogleForm = () => {
+  return (
+    <div className="flex-shrink-0">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdnNK-18zAdW1NQXKqA2fzezO4pc6gL0WlX1ElEhVvMHnyUAg/viewform?embedded=true"
+        width="640"
+        height="700"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+      ></iframe>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen flex inline-block">
+      <GoogleForm />
+      <Accordion />
+    </div>
+  );
+};
+
+export default App;
